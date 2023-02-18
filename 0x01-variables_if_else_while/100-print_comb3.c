953928ade0945e4925d27fe71e522fad3c9d274b
achuban <achuban@outlook.com>
2023-02-18 06:52:05 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Print - the number game
+*
+* Description: 'the program's description'
+* Return: Always 0 (Succuccess)
+*/
+int main(void)
+{
+	int c;
+	int d;
+	int x;
+
+	x = 1;
+	for (c = 0; c < 9; c++)
+	{
+		for (d = x; d < 10; d++)
+		{
+			putchar(48 + c);
+			putchar(48 + d);
+			if (c < 9)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		x++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
