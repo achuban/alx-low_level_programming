3b608ddaaace7d6ab4a04167fcddf8516f08d433
achuban <achuban@outlook.com>
2023-02-18 04:57:14 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Print - the alphabet game
+*
+* Description: 'the program's description'
+* Return: Always 0 (Succuccess)
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
