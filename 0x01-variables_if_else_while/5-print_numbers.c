3ee8acf478e3af83e5b23344b3ee6bd2bf6deae8
achuban <achuban@outlook.com>
2023-02-18 04:17:07 -0800

    the number game

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Print - the number game
+*
+* Description: 'the program's description'
+* Return: Always 0 (Succuccess)
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
