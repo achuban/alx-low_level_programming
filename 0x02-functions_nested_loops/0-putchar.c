5202d5a64d510198c45ad87a437e7096e957cf71
achuban <achuban@outlook.com>
2023-02-21 08:10:28 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Print - the alphabet game
+*
+* Description: 'the program's description'
+* Return: Always 0 (Succuccess)
+*/
+int main(void)
+{
+	putchar(95);
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
