2822ad4316f12304cfd433424cf4ae2ea0f4eb4e
achuban <achuban@outlook.com>
2023-02-22 10:52:02 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_last_digit - Print - the alphabet game
+*
+* @c: gets int value
+* Description: 'print_alphabet program's description'
+* Return: Always 0 (Succuccess)
+*/
+int _islower(int c)
+{
+	int r;
+
+	r = c % 10;
+
+	return (r);
+}
