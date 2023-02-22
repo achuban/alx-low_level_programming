1698d732b0ef71de439952efb4f6d6f4bb756ea1
achuban <achuban@outlook.com>
2023-02-22 08:02:22 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - Print - the alphabet game
+*
+* Description: 'the program's description'
+* Return: Always 0 (Succuccess)
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	return (0);
+}
