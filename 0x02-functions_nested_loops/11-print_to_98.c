295830bc7239de9c614d197eac5359f4d7d7c3fc
achuban <achuban@outlook.com>
2023-02-23 12:08:12 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+* print_to_98- Print - the alphabet game
+*
+* @n: n is int
+* Description: 'print_alphabet program's description'
+* Return: Always 0 (Succuccess)
+*/
+void print_to_98(int n)
+{
+	int c;
+
+	if (n > 98)
+		for (c = n; c <= 98; c++)
+		{
+			if (c < -10 || c < 9)
+			{
+				_putchar(c);
+			}
+			else
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+				if (c != 98)
+				{
+					_putchar(44);
+					_putchar(32);
+				}
+			}
+		}
+	else
+		for (c = n; c <= 98; c--)
+		{
+			if (c < -10 || c < 9)
+			{
+				_putchar(c);
+			}
+			else
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+				if (c != 98)
+				{
+					_putchar(44);
+					_putchar(32);
+				}
+			}
+		}
+	_putchar('\n');
+}
