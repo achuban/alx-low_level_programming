8655b931e5b250ac2d947fc6d330840d9a220d52
achuban <achuban@outlook.com>
2023-02-23 13:44:53 -0800

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+* print_times_table - Print - the alphabet game
+*
+* @n: n is int
+* Description: 'print_alphabet program's description'
+* Return: Always 0 (Succuccess)
+*/
+void print_times_table(int n)
+{
+	int c;
+	int r;
+	int m;
+
+	if (n > 0 && n < 16)
+		for (c = 0; c < n; c++)
+		{
+			for (r = 0; r < n; r++)
+			{
+				m = c * r;
+				if (m < 10)
+				{
+					_putchar(m + '0');
+					if (r < 9)
+					{
+						_putchar(44);
+						_putchar(32);
+						if ((r + 1) * c < 10)
+							_putchar(32);
+					}
+				}
+				else
+				{
+					_putchar((m / 10) + '0');
+					_putchar((m % 10) + '0');
+					if (r < 9)
+					{
+						_putchar(44);
+						_putchar(32);
+					}
+				}
+			}
+			_putchar('\n');
+		}
+}
