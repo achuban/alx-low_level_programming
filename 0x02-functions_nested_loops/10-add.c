1eb14876154ef774fc9b22a89ddb518c4b62d362
achuban <achuban@outlook.com>
2023-02-23 10:31:04 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* add - Print - the alphabet game
+*
+* @a: int input 1
+* @b: int input 2
+* Description: 'print_alphabet program's description'
+* Return: Always 0 (Succuccess)
+*/
+int add(int a, int b)
+{
+	return a + b;
+}
