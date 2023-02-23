e3763245d879c70481f69547b909c10f99335dbd
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-24 01:13:58 +0300

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,42 +15,47 @@ void print_times_table(int n)
 	if (n > 0 && n < 16)
 		for (c = 0; c <= n; c++)
 		{
-			for (r = 0; r <= n; r++)
-			{
-				m = c * r;
-				if (m < 10)
-				{
-					_putchar(m + '0');
-					if (r < n)
-					{
-						_putchar(44);
-						_putchar(32);
-						if ((r + 1) * c < 10)
-							_putchar(32);
-					}
-				}
-				else if (m > 10 && m < 100)
-				{
-					_putchar((m / 10) + '0');
-					_putchar((m % 10) + '0');
-					if (r < n)
-					{
-						_putchar(44);
-						_putchar(32);
-					}
-				}
-				else if (m > 100)
-				{
-					_putchar((m / 100) + '0');
-					_putchar((m / 10) + '0');
-					_putchar((m % 10) + '0');
-					if(r < n)
-					{
-						_putchar(44);
-						_putchar(32);
-					}
-				}
-			}
+			prow(n, c, r);
 			_putchar('\n');
 		}
+}
+
+void prow(int n, int c, int r)
+{
+	for (r = 0; r <= n; r++)
+	{
+		m = c * r;
+		if (m < 10)
+		{
+			_putchar(m + '0');
+			if (r < n)
+			{
+				_putchar(44);
+				_putchar(32);
+				if ((r + 1) * c < 10)
+					_putchar(32);
+			}
+		}
+		else if (m > 10 && m < 100)
+		{
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			if (r < n)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+		}
+		else if (m > 100)
+		{
+			_putchar((m / 100) + '0');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			if(r < n)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+		}
+	}	
 }
