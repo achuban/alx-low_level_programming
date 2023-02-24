cd43dee6b139aad9fd6069aa0ab77c7fbe06fb4f
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-24 14:11:35 +0300

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,34 +10,39 @@
 void print_times_table(int n)
 {
 	if (n > 0 && n < 16)
-		prow(n);
-}
-
-void prow(int n)
-{
-	int c;
+		int c;
 	int r;
 	int m;
+	int n;
 	
 	for (c = 0; c <= n; c++)
 	{
 		for (r = 0; r <= n; r++)
 		{
 			m = c * r;
-			printf("%d, ", m);
-			if (m < 10)
-			{
-				
-			}
-			else if (m >= 10 && m < 100)
-			{
-				
-			}
-			else if (m >= 100)
-			{
-				
-			}
+			n = (c +1) * r ;
+			printf("%d", m);
+			prow(n, r);
 		}
 		_putchar('\n');
+	}
+}
+
+void prow(int n, int r)
+{
+	if(r < n)
+	{
+		if (n < 10)
+		{
+			printf(",   ");
+		}
+		else if (n >= 10 && n < 100)
+		{
+			printf(",  ");
+		}
+		else if (n >= 100)
+		{
+			printf(", ");
+		}
 	}
 }
