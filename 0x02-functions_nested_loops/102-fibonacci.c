cc1712f8931a8f3790e5fef4525feac0667cc914
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-24 18:25:35 +0300

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,31 +7,31 @@
 */
 int main(void)
 {
-int i;
-int f;
-int p1;
-int p2;
+	int i;
+	int f;
+	int p1;
+	int p2;
 
-p1 = 1;
-p2 = 0;
+	p1 = 1;
+	p2 = 0;
 
-while (i <= 50)
-{
-if (i != 50)
-{
-printf("%d, ", p2);
-printf("%d, ", p1);
-}
-else
-{
-printf("%d", p2);
-printf("%d", p1);
-}
-f = p1 + p2;
-p2 = p1;
-p1 = f;
-}
-printf("\n");
+	while (i <= 50)
+	{
+		if (i != 50)
+		{
+			printf("%d, ", p2);
+			printf("%d, ", p1);
+		}
+		else
+		{
+			printf("%d", p2);
+			printf("%d", p1);
+		}
+		f = p1 + p2;
+		p2 = p1;
+		p1 = f;
+	}
+	printf("\n");
 
-return (0);
+	return (0);
 }
