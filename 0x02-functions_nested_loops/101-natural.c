a4aab7ee3eaa9289e21ea7ab81ced91fcee0c396
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-24 16:09:43 +0300

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - multiples of 3 or 5
+*
+* * Description: 'print_ multiples of 3 or 5  below 1024 (excluded)'
+* Return: Always 0 (Succuccess)
+*/
+int main(void)
+{
+    int i;
+    int s;
+    
+    for (i = 0; i < 1024; i++)
+    {
+        if (i % 3 == 0)
+            s += i;
+        else if (i % 5 == 0)
+            s += i;
+    }
+    printf("%d", s);
+    printf("\n");
+    
+    return (1);
+}
