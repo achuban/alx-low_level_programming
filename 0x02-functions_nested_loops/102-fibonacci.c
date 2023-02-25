b6e0f6a0785464bdaf27d0bd2d7f843bb3f5f93e
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-25 16:43:49 +0300

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 	int i;
-	int f;
-	int p1;
-	int p2;
+	long f;
+	long p1;
+	long p2;
 
 	p1 = 2;
 	p2 = 1;
@@ -19,7 +19,7 @@ int main(void)
 	printf("%d, ", p2);
 	printf("%d, ", p1);
 
-	while (i <= 50)
+	while (i < 49)
 	{
 		f = p1 + p2;
 		p2 = p1;
