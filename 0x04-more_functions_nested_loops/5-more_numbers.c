e2a878ccf498d9c0d16e78d9de9a6de610caf65e
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-26 19:25:38 +0300

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* more_numbers - Print - nubers 0-14
+*
+* Description: 'print numbers from 0-14'
+*/
+void more_numbers(void)
+{
+	int c;
+	int r;
+
+	for (c = 0; c < 10; c++)
+	{
+		for  (r = 0; r < 14; r++)
+		{
+			_putchar(r + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
