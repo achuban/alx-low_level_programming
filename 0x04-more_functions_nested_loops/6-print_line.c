78a75870b6e951a29f2cdfafa54579b86908a582
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-26 21:07:47 +0300

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_line - Print - line
+*
+* @n: int number of line
+* Description: 'just print line'
+*/
+void print_line(int n)
+{
+	for (c = 0; c < n; c++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
