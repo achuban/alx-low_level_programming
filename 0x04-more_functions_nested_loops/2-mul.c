f02ce190c0a8ed03694cdd518b6360382b61967f
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-26 18:39:46 +0300

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* mul - multiply numbers 
+*
+* @a: gets int value
+* @b: gets int value
+* Description: 'multiply two numbers'
+* Return: Always 0 (Succuccess)
+*/
+
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+
+	return (r);
+}
