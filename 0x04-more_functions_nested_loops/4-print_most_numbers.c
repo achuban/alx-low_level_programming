096812892b7e53d115a1f299d7f18d9a8e98994f
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-26 19:16:56 +0300

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_most_numbers - Print - nubers 0-9 ot 2&4
+*
+* Description: 'print numbers from0-9 but not 2 || 4'
+*/
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		if (c != 2 || c != 4)
+			_putchar(48 + c);
+	}
+	_putchar('\n');
+}
