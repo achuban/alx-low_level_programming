be1e50ce90f34b7182932364aa46f5e962f833bb
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-26 17:54:36 +0300

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
-* _islower - Print - the alphabet game
+* _isupper - checks if the input is upper case - the alphabet game
 *
 * @c: gets int value
-* Description: 'print_alphabet program's description'
+* Description: 'checks if the input is upper case - the alphabet game'
 * Return: Always 0 (Succuccess)
 */
-int _islower(int c)
+int _isupper(int c)
 {
 	int r;
 
 	r = 0;
-	if (c > 96 && c < 123)
+	if (c > 65 && c < 90)
 		r = 1;
 
 	return (r);
