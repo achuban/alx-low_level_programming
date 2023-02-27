5db85272ee56399c6ae0eca423d7fd829312f2f1
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-27 17:38:05 +0300

    FiZZ-BuZZ

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,16 +13,24 @@ int main(void)
 	for (c = 1; c <= 100; c++)
 	{
 		if ((c % 3 == 0) && (c % 5 == 0))
+		{
 			printf("FizzBuzz ");
+		}
 		else if (c % 3 == 0)
+		{
 			printf("Fizz ");
+		}
 		else if (c % 5 == 0)
+		{
 			if (c == 100)
 				printf("Buzz");
 			else
 				printf("Buzz ");
+		}
 		else
+		{
 			printf("%d ", c);
+		}
 	}
 	printf("\n");
 
