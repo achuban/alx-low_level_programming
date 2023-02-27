11e6449b82b168fde5d1e0bc37439375c2fcea25
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-27 16:14:02 +0300

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_square - Print - square
+*
+* @size: int n parameter
+* Description: 'print square with #'
+*/
+void print_square(int size)
+{
+	int c;
+	int r;
+
+	for (c = 0; c < size; c++)
+	{
+		for (r = 0; r < size; r++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
