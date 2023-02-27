8b4a53eed76dd5dd04dcb6460476e0b9e1592d8d
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-27 15:28:28 +0300

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_diagonal - Print - diagonal
+*
+* @n: int n parameter 
+* Description: 'print_diagonal with \'
+*/
+void print_diagonal(int n)
+{
+	int c;
+	int r;
+
+	for (c = 0; c < n; c++)
+	{
+		for (r = 0; r < n - 1; r++)
+		{
+			_putchar(32);
+		}
+		_putchar(92);
+	}
+	_putchar('\n');
+}
