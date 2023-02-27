1850fb371faf307d9de689d6fd668a88efe5204e
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-27 18:04:43 +0300

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* print_triangle - Print - triangle
+*
+* @size: int n parameter
+* Description: 'print_triangle with #'
+*/
+void print_triangle(int size)
+{
+	int c;
+	int r;
+
+	for (c = 0; c < size; c++)
+	{
+		for (r = 0; r < size; r++)
+		{
+			if (r < (size - c))
+				_putchar(32);
+			else
+				_putchar(92);
+		}
+		_putchar('\n');
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
