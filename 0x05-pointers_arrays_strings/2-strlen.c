246a00991307836f5e23113709b9acedd6fe8761
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-28 19:29:24 +0300

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* reset_to_98 - reset to 98
+*
+* @n: n is a ponter to int data type
+* Description: 'reset to 98 '
+* Return: int value 0 (Succuccess)
+*/
+int _strlen(char *s)
+{
+	int c;
+	
+	for (i = 0; s[i] != '\0'; i++)
+		
+	return (i)
+}
