b4d803f72b9757eb7604821ac482573ac789aaa4
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-01 00:09:52 +0300

    why

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,11 +14,10 @@ void print_array(int *a, int n)
 	
 	for (i = 0; i < n - 1; i++)
 	{
-		printf("%d", a[i]);
+		printf("%d"a[i]);
 		if (i != (n - 2))
 		{
-			_putchar(44);
-			_putchar(32);
+			printf(", ");
 		}
 	}
 	_putchar('\n');
