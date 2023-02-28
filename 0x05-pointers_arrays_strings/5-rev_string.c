180a7c96a3a9d4a5084a12e5293b4770d42b7b6a
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-28 21:10:34 +0300

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,3 @@
-#include <string.h>
 #include "main.h"
 /**
 * rev_string - reverse string
@@ -12,17 +11,19 @@ void rev_string(char *s)
 
 	int i;
 	int j;
-	char *c;
+	char c;
 	
 	j = 0;
+	c = s[0];
 	
 	for (i = 0; s[i] != '\0'; i++)
 	{
 	}
-	strcpy(c, *s);
 	for (i = i - 1; i >= 0; i--)
 	{
-		s[j] = c[i];
+		c = s[j];
+		s[j] = s[i];
+		s[i] = c;
 		j++;
 	}
 }
