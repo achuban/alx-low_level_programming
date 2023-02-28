60dc51116cf62c45bf77f3154516396b959c8c61
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-28 23:47:05 +0300

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+include "main.h"
+/**
+* print_array - prints print_array
+*
+* @a: a is a ponter to int
+* @n: n is int
+* Description: 'prints print_array'
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		_putchar(str[i]);
+		if (i != n - 1)
+		{
+			_putchar(44);
+			_putchar(32);
+		}
+	}
+	_putchar('\n');
+}
