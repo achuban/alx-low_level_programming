61fb3317d7e0a83473ce17c73f890cf77f8fca6a
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-28 19:56:41 +0300

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _puts - reset to 98
+*
+* @s: s is a ponter to char data type
+* Description: 'count the length'
+*/
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+
+	return (i);
+}
