9619b7fd20532c522fe3b9152b698ba8fc87496b
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-01 00:23:27 +0300

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _strcpy - prints string with a new line at the end
+*
+* @dest: s is a ponter to char dest
+* @src: src is a pointer to char src
+* Description: 'prints string with a new line at the end '
+* Return dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (*dest);
+}
