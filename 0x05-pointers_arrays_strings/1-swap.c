2eb1c8563a2e7d941de28d7b9f75b0133c6faa88
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-28 19:15:38 +0300

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* reset_to_98 - reset to 98
+*
+* @n: n is a ponter to int data type
+* Description: 'reset to 98 '
+* Return: Always 0 (Succuccess)
+*/
+
+void swap_int(int *a, int *b)
+{
+	int s;
+	
+	s = *a;
+	*a = *b;
+	*b = s;
+}
