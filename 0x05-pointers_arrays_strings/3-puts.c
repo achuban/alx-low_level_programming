369ed0a787e8c1a71037e77b7c35c49ad65e7d59
achuban <achuban@outlook.com>
2023-02-28 09:08:53 -0800

    remov space

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
-* _puts - prints string with a new line at the end 
+* _puts - prints string with a new line at the end
 *
 * @str: s is a ponter to sting
 * Description: 'prints string with a new line at the end '
 */
+
 void _puts(char *str)
 {
+
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
