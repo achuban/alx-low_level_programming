d057bacc5343e32c5efdcfe906111b0762fbe8fc
AchuBan <57764327+achuban@users.noreply.github.com>
2023-02-28 22:50:36 +0300

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* puts2 - a function that prints every other character of a string
+*
+* @str: s is a ponter to sting
+* Description: 'a function that prints every other character of a string, starting with the first character, followed by a new line '
+*/
+
+void puts2(char *str)
+{
+	int i;
+	int j;
+
+	j = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (j = 0)
+		{
+			_putchar(str[i]);
+			j = 1;
+		}
+		else
+		{
+			j = 0;
+		}
+	}
+	_putchar('\n');
+}
