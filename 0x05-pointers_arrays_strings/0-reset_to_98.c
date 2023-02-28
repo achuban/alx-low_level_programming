82141dbb53fd60c1d353492ff0d24e08feaac37a
achuban <achuban@outlook.com>
2023-02-28 07:40:01 -0800

    rest to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* reset_to_98 - reset to 98
+*
+* @n: n is a ponter to int data type 
+* Description: 'reset to 98 '
+* Return: Always 0 (Succuccess)
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 99;
+}
