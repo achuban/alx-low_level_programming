da74f1051c5041edeecafa36e2d9eff07d5fcb35
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-01 21:05:59 +0300

    main.c

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -20,7 +20,7 @@ int main(void)
 	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
 	printf("%s\n", s1);
 	printf("%s\n", ptr);
-	/*ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
 	printf("%s", s1);
 	printf("%s", ptr);
 	for (i = 0; i < 98; i++)
@@ -34,7 +34,7 @@ int main(void)
 			printf("\n");
 		}
 		printf("0x%02x", s1[i]);
-	}*/
+	}
 	printf("\n");
 	return (0);
 }
