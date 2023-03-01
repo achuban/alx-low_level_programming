2f4c3903d93517f73513c994c3cbf5a4727d98c9
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-01 20:08:45 +0300

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,11 +4,9 @@
 *
 * @dest: s is a ponter to char dest
 * @src: src is a pointer to char src
-* @n: is int 
-* Description: 'prints string with a new line at the end '
+* @n: is int* Description: 'prints string with a new line at the end '
 * Return: dest
 */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
