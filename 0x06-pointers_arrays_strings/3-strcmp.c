347e9a308b8671b67d6e3e63a57aab0f0aad0c3c
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-02 17:15:35 +0300

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,9 +17,9 @@ int _strcmp(char *s1, char *s2)
 		if (s1[i] != s2[i])
 		{
 			if (s1[i] > s2[i])
-				r = 1;
+				r = 15;
 			else if (s1[i] != s2[i])
-				r = -1;
+				r = -15;
 			break;
 		}
 	}
