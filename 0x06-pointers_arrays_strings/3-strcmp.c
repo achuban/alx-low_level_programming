d494911781045fab51babefcffc80f83caac3035
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-02 16:39:30 +0300

    bug fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,11 +14,11 @@ int _strcmp(char *s1, char *s2)
 	r = 0;
 	for (i = 0; s1[i] != '\0'; i++)
 	{
-		if (s1[i] =! s2[i])
+		if (s1[i] != s2[i])
 		{
 			if (s1[i] > s2[i])
 				r = 1;
-			else if (s1[i] =! s2[i])
+			else if (s1[i] != s2[i])
 				r = -1;
 			break;
 		}
