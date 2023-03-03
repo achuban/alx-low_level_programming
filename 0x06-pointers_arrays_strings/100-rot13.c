4e2f737a4eb1ec70a98c534b4ed8ae5752e677c6
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-03 16:48:48 +0300

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* leet - leet encoding
+*
+* @c: c is a ponter to char dest
+* Return: dest
+*/
+char *rot13(char *c)
+{
+	int i;
+	int j;
+
+	char s1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (c[i] == s1[j])
+				c[i] = s2[j];
+		}
+	}
+	return (c);
+}
