8f5bc68d4376d3bba2dc7d178311875b60d837c7
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-03 15:44:02 +0300

    7-main.c

--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -10,8 +10,6 @@ int main(void)
 	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
 	char *p;
 
-
-	printf("%s", p);
 	printf("%s", s);
 	p = leet(s);
 	printf("%s", p);
