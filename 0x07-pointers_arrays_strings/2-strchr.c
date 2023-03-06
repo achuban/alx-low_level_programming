33efc11f577393ea6533d66ef531a10ed3a34b5b
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-06 19:18:08 +0300

    _strchr - searches for the first occurrence

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _strchr - searches for the first occurrence
+*
+* @c: c is char to be found
+* Description: 'searches for the first occurrence of the character c'
+* Return: s
+*/
+char *_strchr(char *s, char c)
+{
+	unsigned int j;
+
+	for (j = 0; j < n; j++)
+	{
+		if (s[j] == c)
+			return (s[j]);
+	}
+	return ('\0');
+}
