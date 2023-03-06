3ab5c9b07d8214d3803e452b9977584462fedbd3
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-06 21:40:41 +0300

    3-strspn.c  returns the length of the initial substring of the string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* _strspn -  returns the length of the initial substring of the string
+*
+* @s: s is a ponter to char dest
+* @accept: accept is a char to fill
+* Description: 'concatenates two string until n'
+* Return: s
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int c;
+
+	c = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				c++;
+		}
+	}
+	return (c);
+}
