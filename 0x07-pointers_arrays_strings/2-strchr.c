befe8094452c466f99f3d34bff3b080dda2fc16c
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-06 20:00:44 +0300

    checking id array is null

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,14 @@ char *_strchr(char *s, char c)
 {
 	unsigned int j;
 
-	if (c == '\0' || *s == '\0')
+	if (*s == '\0')
 		return (s);
 	for (j = 0; s[j] != '\0'; j++)
 	{
 		if (s[j] == c)
 			return (&s[j]);
 	}
+	if (c == '\0')
+		return (s);
 	return ('\0');
 }
