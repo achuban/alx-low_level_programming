b3fd80ce982f6b89b4fd342293e66a8416f5450c
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-06 21:50:58 +0300

    checking for space

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,7 +14,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int c;
 
 	c = 0;
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != ' ' && s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
