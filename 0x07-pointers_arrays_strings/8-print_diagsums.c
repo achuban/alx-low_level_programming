30f0a8cdaf668aeed693a1d8bb618efc0dfded69
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-07 22:45:40 +0300

    remove line over 80 characters

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* print_diagsums -  prints the sum of the two diagonals of a square matrix of integers
+* print_diagsums - sum of the two diagonals of a square matrix
 *
 * @a: s is a array
 * @size: size
-* Description: 'prints the sum of the two diagonals of a square matrix of integers'
+* Description: 'sum of the two diagonals of a square matrix'
 * Return: void
 */
 void print_diagsums(int *a, int size)
