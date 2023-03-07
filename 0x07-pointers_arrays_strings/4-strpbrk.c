1b1934f879006714d3d82cf959a364e37e3e542e
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-07 17:21:14 +0300

    4-strpbrk.c Updated, checking algorithim

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,19 +11,14 @@ char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i;
 	unsigned int j;
-	int c;
 
-	c = -1;
 	for (i = 0; s[i] != ' ' && s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-				if (c >= 0 && c > i)
-					c = i;
+				return (&s[i]);
 		}
 	}
-	if (c >= 0)
-		return (&s[i]);
 	return ('\0');
 }
