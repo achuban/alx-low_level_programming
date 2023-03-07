9a5e2ae640e86a1a514078c094acf55acf7ce927
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-07 17:13:07 +0300

    4-strpbrk.c finds the first character in the string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* _strpbrk -  finds the first character in the string
+*
+* @s: s is a ponter to char dest
+* @accept: accept is a ponter char to fill
+* Description: 'finds the first character in the string'
+* Return: s
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int c;
+
+	c = -1;
+	for (i = 0; s[i] != ' ' && s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				if (c >= 0 && c > i)
+					c = i;
+		}
+	}
+	if (c >= 0)
+		return (&s[i]);
+	return ('\0');
+}
