cbb55d0c2456446d65394ee83c07358d92db2246
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-08 18:38:05 +0300

    bug fix

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,25 +13,25 @@ int is_palindrome(char *s)
 
 	l = 0;
 	r =  _strlen_recursion(s);
+	if (r == 0)
+		return (1);
 	return (ch_palindrome(s, l, r));
 }
 /**
 * ch_palindrome - check if if a string is a palindrome
 *
 * @s: s is char pointer
-* @l: l is int, leftIndex 
+* @l: l is int, leftIndex
 * @r: r is int, rightIndex
 * Return: int.
 */
 int ch_palindrome(char *s, int l, int r)
 {
-	if(*s == '\0' || l < 0 || r < 0)
-		return 0;
-	if(l >= r)
-		return 1;
-	if(s[l] == s[r])
-		return ch_palindrome(s, l + 1, r - 1);
-	return 0;
+	if (s[l] != s[r])
+		return (0);
+	if (l < r + 1)
+		return (ch_palindrome(s, l + 1, r - 1));
+	return (1);
 }
 /**
 * _strlen_recursion - cout string
