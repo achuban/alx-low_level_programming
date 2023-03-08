fc4a902f2309fcac4852dd4e5ed7501479519c02
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-08 18:11:10 +0300

    create 100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* is_palindrome - check if if a string is a palindrome
+*
+* @s: s is char pointer
+* Return: int.
+*/
+int is_palindrome(char *s);
+{
+	int l;
+	int r;
+
+	l = 0;
+	r =  _strlen_recursion(s);
+
+	if(NULL == s || l < 0 || r < 0)
+		return 0;
+	if(l >= r)
+		return 1;
+	if(s[l] == s[r])
+	{
+		return is_palindrome(s, l + 1, r - 1);
+	}
+	return 0;
+}
