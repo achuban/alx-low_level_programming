7366d7ee95969b2884303423256441d12ca47396
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-08 16:58:56 +0300

    updated function

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,18 +7,8 @@
 */
 int _strlen_recursion(char *s)
 {
-	int i;
-
-	i = 0;
-	if (s[0] == '\0')
-	{
+	if (*s == '\0')
 		return (0);
-	}
-	else
-	{
-		i++;
-		s++;
-		_strlen_recursion(s);
-	}
-	return (i);
+	s++;
+	return (_strlen_recursion(s));
 }
