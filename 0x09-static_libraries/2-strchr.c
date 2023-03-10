a06062bc3673cf466eb93404245017d594c54555
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-10 22:18:10 +0300

    Create 2-strchr.c

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _strchr - searches for the first occurrence
+*
+* @s: s is char pointer
+* @c: c is char to be found
+* Description: 'searches for the first occurrence of the character c'
+* Return: s
+*/
+char *_strchr(char *s, char c)
+{
+	if (*s == '\0')
+		return (s);
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (c == '\0')
+		return (s);
+	return ('\0');
+}
