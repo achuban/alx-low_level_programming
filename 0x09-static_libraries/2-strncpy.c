be9b4dc8b3f67c724c15d3f99a75ec3a47029552
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-10 22:18:55 +0300

    Create 2-strncpy.c

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* _strncpy - prints string with a new line at the end
+*
+* @dest: s is a ponter to char dest
+* @src: src is a pointer to char src
+* @n: is int* Description: 'prints string with a new line at the end '
+* Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		if (src[i] == '\0')
+			break;
+		dest[i] = src[i];
+	}
+	for (j = i; j < n; j++)
+	{
+		dest[j] = '\0';
+	}
+	/*dest[i] = '\0';*/
+	return (dest);
+}
