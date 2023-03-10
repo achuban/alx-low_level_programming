d4871635f41913f2f68f73ff40d4f8de8602be82
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-10 21:55:36 +0300

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,1 +1,19 @@
+#include "main.h"
+/**
+* _isupper - checks if the input is upper case - the alphabet game
+*
+* @c: gets int value
+* Description: 'checks if the input is upper case - the alphabet game'
+* Return: Always 0 (Succuccess)
+*/
 
+int _isupper(int c)
+{
+	int r;
+
+	r = 0;
+	if (c >= 65 && c <= 90)
+		r = 1;
+
+	return (r);
+}
