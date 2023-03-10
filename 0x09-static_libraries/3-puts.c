b1ff0bd925b431b2d201e8fc59f7e1a98cf4df05
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-10 22:19:36 +0300

    Create 3-puts.c

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _puts - prints string with a new line at the end
+*
+* @str: s is a ponter to sting
+* Description: 'prints string with a new line at the end '
+*/
+
+void _puts(char *str)
+{
+
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
