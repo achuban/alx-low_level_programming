bd7288949d364ad37f87468c2e643e90c61ca866
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-10 22:07:59 +0300

    Create 1-isdigit.c

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _isdigit - checks if the input _is digit
+*
+* @c: gets int value
+* Description: 'checks if the input is is digit'
+* Return: Always 0 (Succuccess)
+*/
+
+int _isdigit(int c)
+{
+	int r;
+
+	r = 0;
+	if (c >= 48 && c <= 57)
+		r = 1;
+
+	return (r);
+}
