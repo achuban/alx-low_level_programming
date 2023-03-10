d665ea83de751a08e2a2c58aaa5a711f3f2fb2a0
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-10 22:09:42 +0300

    Create 2-strlen.c

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _strlen - reset to 98
+*
+* @s: s is a ponter to char data type
+* Description: 'count the length'
+* Return: int value 0 (Succuccess)
+*/
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
