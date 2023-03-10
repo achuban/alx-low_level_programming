9001b2511a50d8639b516cfabe2adb13a1a8ea3d
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-10 22:11:46 +0300

    Create 3-strcmp.c

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* _strcmp - compare two string
+*
+* @s1: s1 first param, is a pointer to char
+* @s2: s2 second param, is a pointer to char
+* Return: int
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int r;
+
+	r = 0;
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			r = s1[i] - s2[i];
+			break;
+		}
+	}
+	return (r);
+}
