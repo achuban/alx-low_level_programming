d8c57b51153900068be64446508b8a1cefa56586
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-10 22:14:04 +0300

    Create 5-strstr.c

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+* _strstr -  finds the first occurrence of the substring
+*
+* @haystack: haystack is a ponter to char to searched
+* @needle: needle is a ponter char to find
+* Description: 'finds the first occurrence of the substring'
+* Return: s or Null
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+	int s;
+	int t;
+
+	i = 0;
+	j = 0;
+	t = 0;
+	while (needle[j] != '\0')
+		j++;
+	while (haystack[i] != '\0')
+		i++;
+	if (j == 0)
+		return (haystack);
+	for (k = 0; haystack[k] != '\0'; k++)
+	{
+		if (haystack[k] == needle[0])
+		{
+			s = k;
+			t = 0;
+			for (l = 0; l < j; l++)
+			{
+				if (haystack[k + l] == needle[l])
+					t++;
+			}
+		}
+		if (t == j)
+			return (&haystack[s]);
+	}
+	return ('\0');
+}
