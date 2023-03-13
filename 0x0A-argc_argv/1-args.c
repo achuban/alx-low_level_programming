6afe0c0a9eb780de7df14e73146a95a6cd4da520
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-13 15:47:59 +0300

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints string with a new line at the end
+*
+* @argc: s is a ponter to sting
+* @argv: s is a ponter to sting
+* Description: 'prints string with a new line at the end '
+* Return: always 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	argc++;
+	return (0);
+}
