977839fa8cbace26b6185eeb380d678bc5b89148
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-13 18:51:43 +0300

    Create 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _strdup - returns a pointer to a newly copied str
+*
+* @str: size is char pointer
+* Description: 'returns a pointer to a newly copied str'
+* Return: char
+*/
+char *_strdup(char *str)
+{
+	char *s;
+	int i;
+	int j;
+
+	i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		i++;
+	}
+	i++;
+	s = malloc(i * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (j = 0; j <= i; j++)
+	{
+		s[j] = str[j];
+	}
+	return (res);
+}
