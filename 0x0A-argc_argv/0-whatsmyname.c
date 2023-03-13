7a0bc5771e6bd5879a0d5ff2f4f39b7058411ed6
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-13 15:44:54 +0300

    Update 0-whatsmyname.c betty style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,6 +6,7 @@
 * @argc: s is a ponter to sting
 * @argv: s is a ponter to sting
 * Description: 'prints string with a new line at the end '
+* Return: always 0
 */
 int main(int argc, char *argv[])
 {
