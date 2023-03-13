2c2ec52adf377bc70d7b2bf3c8327993d74a6ffc
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-13 15:23:54 +0300

    Update 0-whatsmyname.c betty style updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* _puts - prints string with a new line at the end
+* main - prints string with a new line at the end
 *
-* @str: s is a ponter to sting
+* @argc: s is a ponter to sting
+* @argv: s is a ponter to sting
 * Description: 'prints string with a new line at the end '
 */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv[0]);
+	printf("%s\n", argv[0]);
 }
