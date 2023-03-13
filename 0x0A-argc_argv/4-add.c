75356f6d81bb819eb6c00ed1e0410f45ed4420e0
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-13 16:30:19 +0300

    Create 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints string with a new line at the end
+*
+* @argc: s is a ponter to sting
+* @argv: s is a ponter to sting
+* Description: 'prints string with a new line at the end '
+* Return: always 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+	int s;
+	char *l;
+
+	s = 0;
+	if (argc == 0)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		l = argv[i];
+		while (argv[i] != '\0')
+		{
+			if (*l < 47 || *l > 57)
+			{
+				printf("%s\n", c);
+				return (1);
+			}
+			l++;
+		}
+		s += argv[i];
+	}
+	printf("%i\n", s);
+	return (0);
+}
