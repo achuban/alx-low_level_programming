fb3ae8bec1cd0946fcad6023aa15efb42d317936
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-13 15:14:48 +0300

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* _puts - prints string with a new line at the end
+*
+* @str: s is a ponter to sting
+* Description: 'prints string with a new line at the end '
+*/
+int main(int argc, char **argv)
+{
+	printf("%s\n", *argv[0]);
+}
