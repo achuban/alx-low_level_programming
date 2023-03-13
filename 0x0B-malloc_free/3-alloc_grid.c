dc8813124669b08500f342fd5aad5845168568a8
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-13 19:36:55 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,6 +22,15 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		s[i] = malloc(width * sizeof(int));
+		if (s[i] == NULL)
+		{
+			for (; i >= 0; i--)
+			{
+				free(s[i]);
+			}
+			free(s);
+			return (NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
 	{
