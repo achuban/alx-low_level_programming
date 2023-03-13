c6417941475ab5ba37107cfba53f2f2254b0edde
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-13 16:00:12 +0300

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints string with a new line at the end
+*
+* @argc: s is a ponter to sting
+* @argv: s is a ponter to sting
+* Description: 'prints string with a new line at the end '
+* Return: always 0
+*/
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%i\n", argv[1] * argv[2]);
+	return (0);
+}
