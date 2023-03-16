2821a1b201114713c1ef3d38af49a109bf8cd5bf
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-17 00:18:42 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,7 +21,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return (ptr);
+		return (NULL);
 	}
 	if (ptr == NULL && new_size != 0)
 	{
