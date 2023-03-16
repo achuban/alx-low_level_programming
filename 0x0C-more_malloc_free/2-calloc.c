01434a08b6657aae6e795cc5eeea4da9a5c5c9a2
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-16 22:31:54 +0300

    Create 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _calloc - allocates memory for an array, using malloc
+*
+* @nmemb: nmemb is unsigned int
+* @size: size is unsigned int
+* Description: 'allocates memory for an array, using malloc'
+* Return: pointer
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *s;
+	int i;
+	int j;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	s = malloc(nmemb * size);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+		s[i] = 0;
+	}
+	return (s);
+}
