3b740292515ea8edeabd1ef778d75f875b1f6cdd
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-16 21:59:00 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,10 +12,10 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	int i;
-	int k;
-	int j;
-	int m;
+	unsigned int i;
+	unsigned int k;
+	unsigned int j;
+	unsigned int m;
 
 	i = 0;
 	k = 0;
