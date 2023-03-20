7277992f9b494cbaf22a3eaebc47746c98e0e61a
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-20 20:04:49 +0300

    Update 4-new_dog.c betty style

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,7 +26,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		j++;
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
-		   return (NULL);
+		return (NULL);
 	d->name = malloc(sizeof(char) * (i + 1));
 	if (d->name == NULL)
 	{
