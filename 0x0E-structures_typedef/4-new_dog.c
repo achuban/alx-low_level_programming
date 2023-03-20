26aa1de6ecb305f89c5ac97f3eb0ee422138e8d7
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-20 20:21:52 +0300

    Update 4-new_dog.c order of code

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -33,8 +33,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(d);
 		return (NULL);
 	}
-	for (k = 0; k <= i; k++)
-		d->name[k] = name[k];
 	d->age = age;
 	d->owner = malloc(sizeof(char) * (j + 1));
 	if (d->owner == NULL)
@@ -43,6 +41,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(d);
 		return (NULL);
 	}
+	for (k = 0; k <= i; k++)
+		d->name[k] = name[k];
 	for (k = 0; k <= i; k++)
 		d->owner[k] = owner[k];
 	return (d);
