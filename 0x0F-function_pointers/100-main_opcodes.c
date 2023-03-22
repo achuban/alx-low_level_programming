5e059b852e67a6270821c1b4eaa258e4062b7c24
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-22 19:52:22 +0300

    Create 100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+* main - prints the opcodes of its own main function
+*
+* @argc: argc is count of arg
+* @argv: argv is a ponter to arg value
+* Description: 'prints the opcodes of its own main function'
+* Return: int
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	
+	return (0);
+}
