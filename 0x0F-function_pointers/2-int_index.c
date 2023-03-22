0d5767b699ac18ab78e209a7ef00ed787fb8bcda
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-22 17:52:33 +0300

    Create 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+* int_index - searches for an integer
+*
+* @array: array is array of int
+* @size: size is second parm
+* @cmp: action is pointer to function
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int r;
+
+	r = -1;
+	if (array == NULL || size == 0 || cmp == NULL)
+		return;
+	if (size <= 0)
+		return (r);
+	for (i = 0; i < size; i++)
+	{
+		if(cmp(array[i]) != 0)
+			return (i);
+	}
+	return (r);
+}
