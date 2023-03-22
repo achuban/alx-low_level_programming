655eef2bb10f909e59367a38780d50feed936c40
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-22 16:55:26 +0300

    Create 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+* print_name -  print_name
+*
+* @name: name is first param
+* @f: f is second parm
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
