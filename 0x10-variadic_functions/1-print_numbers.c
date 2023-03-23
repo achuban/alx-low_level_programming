df00c0220107736ed49cbff751963efe007e14d3
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-23 18:17:15 +0300

    Create 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+* print_numbers -  print numbers
+*
+* @separator: separator is first param
+* @n: n is second param
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	if (separator != NULL)
+	{
+		va_start(ap, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(ap, int));
+			if (i == (n - 1))
+				printf("%c", separator);
+		}
+		va_end(ap);
+		printf('\n');
+	}
+}
