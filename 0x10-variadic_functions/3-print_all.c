16bb30b01e2460365389decab7884a91fe4ab148
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-23 19:39:43 +0300

    Create 3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+* print_all -  print numbers
+*
+* @format: format is first param
+*/
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *s;
+
+	va_start(ap, format);
+	while ((arg = va_arg(ap, char *)))
+	{
+		s = va_arg(ap, char *);
+		if (format == NULL)
+			printf("(nil)");
+		else
+			printf("%format", s);
+	}
+	va_end(ap);
+	printf("\n");
+}
