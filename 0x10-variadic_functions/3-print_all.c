63750ae8ba313b37d932cfa8ea733c0b1ebd3ad9
AchuBan <57764327+achuban@users.noreply.github.com>
2023-03-23 19:46:18 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -19,7 +19,7 @@ void print_all(const char * const format, ...)
 		if (format == NULL)
 			printf("(nil)");
 		else
-			printf("%format", s);
+			printf("%"(*format), s);
 	}
 	va_end(ap);
 	printf("\n");
